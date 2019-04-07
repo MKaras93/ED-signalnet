e5d8af0d44ffbe42bf8b0a2088cbe878506df5c0
miczi <miczipl@gmail.com>
2019-04-07 09:07:05 +0200

    Create model SpaceSignal; create basic tests for SpaceSignal model; set timezone

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class SpaceSignal(models.Model):
+    system = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(default=timezone.now())
+    author = models.CharField(max_length=50)
+    content = models.TextField(max_length=140) # TODO: force min length 50 characters.
+
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import SpaceSignal
+from rest_framework.test import APITestCase
+from django.utils import timezone
 
-# Create your tests here.
+
+class SpaceSignalModelTest(TestCase):
+    def setUp(self):
+        SpaceSignal.objects.create(publish_date=timezone.datetime(2019, 1, 1), system='Sol', content='Hello Worlds!', author='God')
+        SpaceSignal.objects.create(publish_date=timezone.datetime(2019, 1, 2), system='Sol', content='Hello again Worlds!', author='God')
+
+    def test_signals_created(self):
+        self.assertEqual(SpaceSignal.objects.all().count(), 2)
+        signal1 = SpaceSignal.objects.get(content='Hello Worlds!')
+        self.assertEqual(signal1.system, 'Sol')
+        self.assertEqual(signal1.author, 'God')
+
+        signal2 = SpaceSignal.objects.get(content='Hello again Worlds!')
+        self.assertEqual(signal2.system, 'Sol')
+        self.assertEqual(signal2.author, 'God')
+
+    def test_string_representation(self):
+        self.fail('TODO')
+
+    def test_object_delete(self):
+        self.fail('TODO')
+
+    def test_object_filtering(self):
+        self.fail('TODO')
+
+
+class APITest(APITestCase):
+    pass
